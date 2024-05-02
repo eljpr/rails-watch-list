1b45fd6ce53467f457dde1d3ea06d2a3e9c8646a
Ella Pierre <ellajpierre@gmail.com>
2024-05-02 17:22:10 +0100

    edited validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :movie, uniqueness: { scope: :list }
+
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'is already in the list' }
   validates :movie, :list, presence: true
   validates :comment, length: { minimum: 6 }
 end
